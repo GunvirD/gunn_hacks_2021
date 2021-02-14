1f53e745b7f092500b17938797ebccb481793c89
GunvirD <72630683+GunvirD@users.noreply.github.com>
2021-02-13 17:02:42 -0800

    Starting - 2
    Added css, updated js server and started putting the theme together

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 var app = express();
 
-app.get("/", function(){
-  app.sendFile('main.html');
+// css location:
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + '/main.html');
 });
 
 app.listen(3000, function(){
-  print("System Running on port 3000")
+  console.log("System Running on port 3000")
 });
